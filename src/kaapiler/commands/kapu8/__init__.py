77322568a06e7b55cd267f5b32bd23e30306299f
DHKold <dhkold@gmail.com>
2025-05-07 21:39:27 +0200

    feat: renaming

--- src/kaapiler/commands/kapu8/__init__.py
+++ src/kaapiler/commands/kapu8/__init__.py
@@ -1,7 +1,21 @@
-from .wait_command import WaitCommand
-from .sync_command import SyncCommand
+from .x0000_wait_command import WaitCommand
+from .x0000_sync_command import SyncCommand
+from .x0001_set_command import SetCommand
+from .x0010_setphase_command import SetPhaseCommand
+from .x0011_setchannel_command import SetChannelCommand
+from .x0100_loop_command import LoopCommand
+from .x0101_jump_command import JumpCommand
+from .x0110_save_command import SaveCommand
+from .x0111_load_command import LoadCommand
 
 __all__ = [
     "WaitCommand",
     "SyncCommand",
+    "SetCommand",
+    "SetPhaseCommand",
+    "SetChannelCommand",
+    "LoopCommand",
+    "JumpCommand",
+    "SaveCommand",
+    "LoadCommand",
 ]
